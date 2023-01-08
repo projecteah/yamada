5d8695f4391e7d714d924bdfb789798560fb540f
Lee <75558996+IlIllIIlIl@users.noreply.github.com>
2023-01-08 15:42:10 +0800

    feat: add chinese

--- i18n.js
+++ i18n.js
@@ -13,6 +13,36 @@ const messages = {
     darkMode: 'Dark',
     lightMode: 'Light',
     noFile: 'No file loaded.'
+  },
+  'zh-Hans-CN': {
+    subtitle: '本地音乐播放器',
+    openFiles: '打开文件',
+    name: '名称',
+    play: '播放',
+    pause: '暂停',
+    prev: '上一首',
+    next: '下一首',
+    repeatOff: '循环：关闭',
+    repeatAll: '循环：列表',
+    repeatOne: '循环：单曲',
+    darkMode: '深色',
+    lightMode: '浅色',
+    noFile: '未加载文件。'
+  },
+  'zh-Hant-TW': {
+    subtitle: '本地音樂播放器',
+    openFiles: '開啟檔案',
+    name: '名稱',
+    play: '播放',
+    pause: '暫停',
+    prev: '上一首',
+    next: '下一首',
+    repeatOff: '循環：關閉',
+    repeatAll: '循環：列表',
+    repeatOne: '循環：單曲',
+    darkMode: '深色',
+    lightMode: '淺色',
+    noFile: '未載入檔案。'
   }
 };
 
